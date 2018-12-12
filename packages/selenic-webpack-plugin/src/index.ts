816b7868016842b5af44b4c83679e2b6d2a9f90f
mkwtys <mkwtys@gmail.com>
2018-12-12 13:46:28 +0900

    chore(webpack-plugin): fix tap name (#8)

--- packages/selenic-webpack-plugin/src/index.ts
+++ packages/selenic-webpack-plugin/src/index.ts
@@ -10,38 +10,35 @@ function readPkgUp(path: string) {
 
 export default class SelenicWebpackPlugin {
   apply(compiler: webpack.Compiler) {
-    compiler.hooks.compilation.tap(
-      'license-header-webpack-plugin',
-      compilation => {
-        compilation.hooks.optimizeChunkAssets.tapAsync(
-          'license-header-webpack-plugin',
-          (chunks, callback) => {
-            chunks.forEach(chunk => {
-              const mainResource = chunk.entryModule.resource
-              const deps: { [key: string]: any } = {}
-              const depsMap = new Map()
-              chunk.modulesIterable.forEach(mod => {
-                if (mod.resource !== mainResource) {
-                  const pkg = readPkgUp(mod.resource)
-                  depsMap.set(`${pkg.name}@${pkg.version}`, pkg)
-                }
-              })
-              depsMap.forEach((v, k) => (deps[k] = v))
-              chunk.files.forEach(filename => {
-                compilation.assets[filename] = new ConcatSource(
-                  createLicenseHeader({
-                    main: readPkgUp(mainResource),
-                    deps
-                  }),
-                  compilation.assets[filename]
-                )
-              })
+    compiler.hooks.compilation.tap('@selenic/webpack-plugin', compilation => {
+      compilation.hooks.optimizeChunkAssets.tapAsync(
+        '@selenic/webpack-plugin',
+        (chunks, callback) => {
+          chunks.forEach(chunk => {
+            const mainResource = chunk.entryModule.resource
+            const deps: { [key: string]: any } = {}
+            const depsMap = new Map()
+            chunk.modulesIterable.forEach(mod => {
+              if (mod.resource !== mainResource) {
+                const pkg = readPkgUp(mod.resource)
+                depsMap.set(`${pkg.name}@${pkg.version}`, pkg)
+              }
             })
+            depsMap.forEach((v, k) => (deps[k] = v))
+            chunk.files.forEach(filename => {
+              compilation.assets[filename] = new ConcatSource(
+                createLicenseHeader({
+                  main: readPkgUp(mainResource),
+                  deps
+                }),
+                compilation.assets[filename]
+              )
+            })
+          })
 
-            callback()
-          }
-        )
-      }
-    )
+          callback()
+        }
+      )
+    })
   }
 }
