8b5f8fed89e332275120c55f79be3be1cecb68af
mkwtys <mkwtys@gmail.com>
2019-11-27 11:15:12 +0900

    format

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-typescript",
+  presets: [
+    '@babel/preset-typescript',
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        "targets": {
-          "node": 8
+        targets: {
+          node: 8
         }
       }
     ]
