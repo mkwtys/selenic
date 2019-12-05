331278950a63c8fea18188acc6d93fbb5898e87a
mkwtys <mkwtys@gmail.com>
2019-12-05 15:50:11 +0900

    remove unnecessary test

--- src/__tests__/extract.test.ts
+++ src/__tests__/extract.test.ts
@@ -2,22 +2,6 @@ import assert from 'assert'
 import { extract } from '../'
 
 describe('@selenic/core: extract', () => {
-  it('minimal', () => {
-    const extracted = extract({
-      name: 'name',
-      version: '0.0.0'
-    })
-    assert(extracted.name)
-    assert(extracted.version)
-  })
-
-  it('add url to license', () => {
-    const extracted = extract({
-      license: 'MIT'
-    })
-    assert.equal(extracted.license, 'MIT')
-  })
-
   it('license type is object', () => {
     const extracted = extract({
       license: {
@@ -32,78 +16,6 @@ describe('@selenic/core: extract', () => {
       licenses: [{ type: 'MIT' }, { type: 'Apache-2.0' }]
     })
     assert.equal(extracted.license, 'MIT, Apache-2.0')
-  })
-
-  it('author', () => {
-    const extracted = extract({
-      author: 'a'
-    })
-    assert.equal(extracted.author, 'a')
-  })
-
-  it('author is person object', () => {
-    let extracted = extract({
-      author: {
-        name: 'a'
-      }
-    })
-    assert.equal(extracted.author, 'a')
-    extracted = extract({
-      author: {
-        name: 'a',
-        email: 'a@example.com'
-      }
-    })
-    assert.equal(extracted.author, 'a <a@example.com>')
-  })
-
-  it('maintainers', () => {
-    const extracted = extract({
-      maintainers: ['m1', 'm2 <m2@example.com>']
-    })
-    assert.equal(extracted.maintainers, 'm1, m2 <m2@example.com>')
-  })
-
-  it('maintainer is person object', () => {
-    const extracted = extract({
-      maintainers: [{ name: 'm1' }, { name: 'm2', email: 'm2@example.com' }]
-    })
-    assert.equal(extracted.maintainers, 'm1, m2 <m2@example.com>')
-  })
-
-  it('maintainers is string', () => {
-    const extracted = extract({
-      maintainers: 'm1, m2 <m2@example.com>'
-    })
-    assert.equal(extracted.maintainers, 'm1, m2 <m2@example.com>')
-  })
-
-  it('contributors', () => {
-    const extracted = extract({
-      contributors: ['c1', 'c2 <c2@example.com>']
-    })
-    assert.equal(extracted.contributors, 'c1, c2 <c2@example.com>')
-  })
-
-  it('contributor is person object', () => {
-    const extracted = extract({
-      contributors: [{ name: 'c1' }, { name: 'c2', email: 'c2@example.com' }]
-    })
-    assert.equal(extracted.contributors, 'c1, c2 <c2@example.com>')
-  })
-
-  it('contributor is string', () => {
-    const extracted = extract({
-      contributors: 'c1, c2 <c2@example.com>'
-    })
-    assert.equal(extracted.contributors, 'c1, c2 <c2@example.com>')
-  })
-
-  it('homepage', () => {
-    const extracted = extract({
-      homepage: 'http://example.com'
-    })
-    assert.equal(extracted.homepage, 'http://example.com/')
   })
 
   it('homepage with hash', () => {
@@ -147,13 +59,6 @@ describe('@selenic/core: extract', () => {
     assert.equal(extracted.homepage, 'http://github.com/#repo-meta-edit')
   })
 
-  it('homepage `github.com` with hash', () => {
-    const extracted = extract({
-      homepage: 'http://github.com#repo-meta-edit'
-    })
-    assert.equal(extracted.homepage, 'http://github.com/#repo-meta-edit')
-  })
-
   it('homepage `github.com` with searchParams', () => {
     const extracted = extract({
       homepage: 'http://github.com?query=string&query2=string2'
@@ -189,12 +94,5 @@ describe('@selenic/core: extract', () => {
       homepage: 'homepage'
     })
     assert.equal(extracted.homepage, 'homepage')
-  })
-
-  it('private package', () => {
-    const extracted = extract({
-      private: true
-    })
-    assert(extracted.private)
   })
 })
